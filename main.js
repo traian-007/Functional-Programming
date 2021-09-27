2d25e7858ee59912bc5074f5dd8487e7a79d64b4
Andrian Gaidarji <traian-007@mail.ru>
2021-09-27 10:43:10 +0300

    some every

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-let arr =[]
-let a = 1
-let b = 0
-let v = 0
-function fibonacciRecursiv(num){
-    arr.push(a)      
-    a = arr[v]+ b 
-    b = arr[v]
-    v++
-    if(num <= 1){
-        return console.log(b);
-    }
-    return fibonacciRecursiv(num - 1) 
-}
+// let arr =[]
+// let a = 1
+// let b = 0
+// let v = 0
+// function fibonacciRecursiv(num){
+//     arr.push(a)      
+//     a = arr[v]+ b 
+//     b = arr[v]
+//     v++
+//     if(num <= 1){
+//         return console.log(b);
+//     }
+//     return fibonacciRecursiv(num - 1) 
+// }
 
-fibonacciRecursiv(6)
+// fibonacciRecursiv(6)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // const arr2 = [1,2,3,4,5,6]
@@ -33,32 +33,32 @@ fibonacciRecursiv(6)
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// function ab(el){
-//     return el+1
-// }
+function ab(el){
+    return el+1
+}
 
-// function ac(el) {
-//     return el===6
-// }
-// const arr =[1,2,3,4,5]
+function ac(el) {
+    return el<5
+}
+const arr =[1,2,3,4,5,6]
 
 
-// const map = (array, fn) => {
-//     return array.reduce((acc,el) => [...acc,fn(el)],[])
-//  }
-// const filter = (array, fn) => {
-//     return array.reduce((acc,el) => fn(el) ? [...acc,el]:[...acc],[])
-//  }
+const map = (array, fn) => {
+    return array.reduce((acc,el) => [...acc,fn(el)],[])
+ }
+const filter = (array, fn) => {
+    return array.reduce((acc,el) => fn(el) ? [...acc,el]:[...acc],[])
+ }
 
-// const some = (array, fn) => {
-//     return array.reduce((acc,el) => fn(el) ? acc = true : acc,false)
-//  }
+const some = (array, fn) => {
+    return array.reduce((acc,el) => fn(el) ? true : acc, false)
+ }
 
-// const every = (array, fn) => {
-//     return array.reduce((acc,el) => fn(el)? acc :acc = false, true)
-//  }
+const every = (array, fn) => {
+    return array.reduce((acc,el) => fn(el)? acc : false, true)
+ }
 
-// console.log(map(arr,ab))
-// console.log(filter(arr,ac))
-// console.log(some(arr,ac))
-// console.log(every(arr,ac))
+console.log(map(arr,ab))
+console.log(filter(arr,ac))
+console.log(some(arr,ac))
+console.log(every(arr,ac))
